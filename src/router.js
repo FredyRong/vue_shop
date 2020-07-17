404b6d3096114746a21275654b2c7850c72b1bdb
FredyRong <419089660@qq.com>
2020-07-17 14:05:33 +0800

    'home'

--- src/router.js
+++ src/router.js
@@ -2,21 +2,36 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './components/Login'
 import Home from './components/Home'
+import Welcome from './components/Welcome'
 
 Vue.use(Router)
 
 const router = new Router({
   routes: [
-    { path: '/', redirect: '/login' },
-    { path: '/login', component: Login },
-    { path: '/home', component: Home}
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [{
+        path: '/welcome',
+        component: Welcome
+      }]
+    }
   ]
 })
 
 router.beforeEach((to, from, next) => {
-  if(to.path === '/login') return next()
+  if (to.path === '/login') return next()
   const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
+  if (!tokenStr) return next('/login')
   next()
 })
 
