bf2ff44d30b63ed9e1a05b83f3c35a3ec9abad86
FredyRong <419089660@qq.com>
2020-07-20 17:30:22 +0800

    完成了权限功能的开发

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import Login from './components/Login'
 import Home from './components/Home'
 import Welcome from './components/Welcome'
 import Users from './components/user/Users'
+import Rights from './components/power/Rights'
+import Roles from './components/power/Roles'
 
 Vue.use(Router)
 
@@ -21,14 +23,24 @@ const router = new Router({
       path: '/home',
       component: Home,
       redirect: '/welcome',
-      children: [{
-        path: '/welcome',
-        component: Welcome
-      },
+      children: [
+        {
+          path: '/welcome',
+          component: Welcome
+        },
         {
           path: '/users',
           component: Users
-        }]
+        },
+        {
+          path: '/rights',
+          component: Rights
+        },
+        {
+          path: '/roles',
+          component: Roles
+        }
+      ]
     }
   ]
 })
@@ -43,6 +55,6 @@ router.beforeEach((to, from, next) => {
 export default router
 
 const originalPush = Router.prototype.push
-  Router.prototype.push = function push (location) {
+Router.prototype.push = function push (location) {
   return originalPush.call(this, location).catch(err => err)
 }
